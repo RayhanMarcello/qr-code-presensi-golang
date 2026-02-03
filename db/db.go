1385bd254cb188370b9c64a3b93eefd32bfe19c4
Rayhan Marcello <rayhanmarcello222@gmail.com>
2026-02-03 19:58:11 +0700

    create connection mysql with orm gorm

---
+++ db/db.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func database() *gorm.DB {
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+
+	if user == "" || pass == "" || host == "" || port == "" || name == "" {
+		fmt.Println("file .env ga berisi data yg bener")
+	}
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		user, pass, host, port, name,
+	)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("err connect to db")
+	}
+	return db
+}
