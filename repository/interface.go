e64fb1b60f686366fd9a18e033ca5a5dc9185498
Rayhan Marcello <rayhanmarcello222@gmail.com>
2026-02-04 14:22:56 +0700

    make interface for each method that i want to use

---
+++ repository/interface.go
@@ -0,0 +1,27 @@
+package repository
+
+import (
+	"presensi-qr-backend/model"
+
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	Create(u *model.User) error
+	FindByEmail(email string) (*model.User, error)
+}
+
+type EventRepository interface {
+	Create(e *model.Event) error
+	FindById(id uint) (*model.Event, error)
+	LastActive() ([]model.Event, error)
+}
+
+type QRSessionRepository interface {
+	Create(q *model.QRSession) error
+	FindByToken(token string) (*model.QRSession, error)
+}
+
+type AttandanceRepository interface {
+	Create(tx *gorm.DB, a *model.Attandance) error
+}
