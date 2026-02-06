bc0941242b7ac7f10ca167896c0a77db2e634376
Rayhan Marcello <rayhanmarcello222@gmail.com>
2026-02-06 17:29:18 +0700

    make interface event

--- service/interface.go
+++ service/interface.go
@@ -5,3 +5,7 @@ import "presensi-qr-backend/model"
 type UserService interface {
 	Create(name, email, password, role string) (*model.User, error)
 }
+
+type EventService interface {
+	CreateEvent(title, desc string, startAt, endAt string, createdBy uint) (*model.Event, error)
+}
