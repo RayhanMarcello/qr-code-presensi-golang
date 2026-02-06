ad103450d227cc1166375e286ad7068e1684dc18
Rayhan Marcello <rayhanmarcello222@gmail.com>
2026-02-06 20:38:52 +0700

    fix bugs

--- service/event_service.go
+++ service/event_service.go
@@ -1,11 +1,44 @@
 package service
 
-import "presensi-qr-backend/repository"
+import (
+	"errors"
+	"presensi-qr-backend/model"
+	"presensi-qr-backend/repository"
+	"time"
+)
 
-type EventService struct {
+var ErrInvalidEventTime = errors.New("invalid event time")
+
+type Service struct {
 	eventRepo repository.EventRepository
 }
 
-func NewEventRepo(eventRepo repository.EventRepository) *EventService {
+func NewEventServer(repo repository.EventRepository) *Service {
+	return &Service{eventRepo: repo}
+}
+
+func (s *Service) CreateEvent(title, desc string, startAt, endAt string, createdBy uint) (*model.Event, error) {
+	start, err := time.Parse(time.RFC3339, startAt)
+	if err != nil {
+		return nil, ErrInvalidEventTime
+	}
+	end, err := time.Parse(time.RFC3339, endAt)
+	if err != nil {
+		return nil, ErrInvalidEventTime
+	}
+
+	e := model.Event{
+		Title:       title,
+		Description: desc,
+		StartAt:     start,
+		EndAt:       end,
+		IsActive:    true,
+		CreatedBy:   createdBy,
+	}
+	if err := s.eventRepo.Create(&e); err != nil {
+		return nil, err
+	}
+
+	return &e, nil
 
 }
